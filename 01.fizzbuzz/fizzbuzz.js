ec01a27d4c5f23980dc278a3abd904422cc850f8
sekito <sekito1107gg@gmail.com>
2024-06-02 17:08:59 +0900

    出力が要件と違ったので修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -4,9 +4,9 @@ for (let i = 1; i <= 20; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   } else if (i % 3 === 0) {
-    console.log("fizz");
+    console.log("Fizz");
   } else if (i % 5 === 0) {
-    console.log("buzz");
+    console.log("Buzz");
   } else {
     console.log(i.toString()); // 出力時に数字はターミナル上で色が変更されるので文字列として出力する
   }
