9967fa115c348966bf34a6e71a052ce1c0a78678
sekito <sekito1107gg@gmail.com>
2024-06-05 23:15:21 +0900

    async/awaitを利用した同期処理プログラムを追加

---
+++ 03.asynchronous/async_await.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import sqlite3 from "sqlite3";
+import { run, get } from "./sqlite_functions.js";
+
+const db = new sqlite3.Database(":memory:");
+
+// エラー無し
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
+const result = await run(db, "INSERT INTO books (title) VALUES (?)", "sample");
+console.log(`id: ${result.lastID}`);
+
+const row = await get(db, "SELECT * FROM books");
+console.log(row);
+
+await run(db, "DROP TABLE books");
+
+// エラー有り
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
+try {
+  await run(db, "INSERT INTO books (price) VALUES (?)", 2980);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
+}
+
+try {
+  await get(db, "SELECT price FROM books");
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
+}
+
+await run(db, "DROP TABLE books");
