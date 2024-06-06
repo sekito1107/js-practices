dbd60aeec998492eee54fa5a83074263582aff89
sekito <sekito1107gg@gmail.com>
2024-06-06 22:35:14 +0900

    出力用データの作成方法を変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -9,28 +9,24 @@ const currentDate = luxon.DateTime.now();
 const year = args.y ?? currentDate.year;
 const month = args.m ?? currentDate.month;
 
-const firstDate = luxon.DateTime.local(year, month, 1);
-const lastDate = firstDate.endOf("month");
+const firstDateOfMonth = luxon.DateTime.local(year, month, 1);
+const lastDateOfMonth = firstDateOfMonth.endOf("month");
 
 let calendar = `\
-      ${firstDate.month}月 ${firstDate.year}
+      ${firstDateOfMonth.month}月 ${firstDateOfMonth.year}
 日 月 火 水 木 金 土
-`;
+${"   ".repeat(firstDateOfMonth.weekday % 7)}`;
 
-calendar += "   ".repeat(firstDate.weekday % 7);
 
-for (let i = 1; i <= lastDate.day; i++) {
+for (let i = 1; i <= lastDateOfMonth.day; i++) {
   calendar += i.toString().padStart(2, " ");
-  if (i === lastDate.day) {
-    break;
-  }
-  if ((i + firstDate.weekday) % 7 === 0) {
+  if (i === lastDateOfMonth.day) {
     calendar += "\n";
   } else {
-    calendar += " ";
+    calendar += (i + firstDateOfMonth.weekday) % 7 === 0 ? "\n" : " ";
   }
 }
 
-calendar += lastDate.weekday === 7 ? "\n" : "\n\n";
+calendar += lastDateOfMonth.weekday === 7 ? "\n" : "\n\n";
 
 process.stdout.write(calendar);
